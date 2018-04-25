aa8a3d0a2959617ede49171bb128e90466978f8f
Louis Mazza <louismazza@gmail.com>
2018-04-25 12:51:36 -0700

    fixed travis issue

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const API_ADDRESS = 'https://bad-apples-api-server.herokuapp.com/' 
-  || 'http://localhost:8888'
+const API_ADDRESS = 'http://localhost:8888' 
+  || 'https://bad-apples-api-server.herokuapp.com/'
 
 
 function createURLObject (base) {
