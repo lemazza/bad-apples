6aa2aac06fc8d42aa2ac8aba4344916186229aa2
Louis Mazza <louismazza@gmail.com>
2018-04-25 14:40:28 -0700

    changed status display test

--- src/components/game-status-display.test.js
+++ src/components/game-status-display.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 
-import GameStatusDisplay from './game-status-display';
+import {GameStatusDisplay} from './game-status-display';
 
 describe('<GameStatusDisplay />', () => {
     it('Renders without crashing', () => {
